a6bc0cb3a21b24a3bb9a63caa8255be6544b1d0a
penglei <penglei@polyv.net>
2024-11-06 13:22:50 +0800

    add a load page button

--- App.tsx
+++ App.tsx
@@ -1,10 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 // App.js
 import React from 'react';
 import { SafeAreaView, StyleSheet, StatusBar, View, Text, Modal, Button, Alert } from 'react-native';
@@ -14,6 +7,7 @@ const App = () => {
   const webViewRef = React.useRef(null);
   const [modalVisible, setModalVisible] = React.useState(false);
   const [message, setMessage] = React.useState('');
+  const [webViewVisible, setWebViewVisible] = React.useState(true);
 
   const generateRandomKey = () => parseInt((Math.random() * 100000).toString(), 10);
   const [key, setKey] = React.useState(generateRandomKey());
@@ -26,6 +20,11 @@ const App = () => {
   const sendMessageToH5 = () => {
     webViewRef.current?.injectJavaScript('receiveMessage("react-native send this message to H5: MSG COMFIRMED!")');
     setModalVisible(false);
+  };
+
+  const loadPage = () => {
+    setWebViewVisible(true); // show WebView
+    setKey(generateRandomKey());
   };
 
   return (
@@ -46,48 +45,48 @@ const App = () => {
           />
         </View>
       </Modal>
-      <WebView
-        source={{ uri: 'https://demo.ipolyv.cn/tanglianghong/test/v3/ReactNativeWebView/?channelId=5005175&lang=en&console=1&#39;'}}
-        // source={{ uri: 'https://www.baidu.com'}}
-        key={key}
-        ref={webViewRef}
-        scalesPageToFit={true}
-        style={{ flex: 1, backgroundColor: 'white' }}
-        startInLoadingState={true}
-        limitsNavigationsToAppBoundDomains={true}
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
-        allowsLinkPreview={true}
-        setSupportMultipleWindows={false}
-        onShouldStartLoadWithRequest={(r) => {
-          return true;
-        }}
-        onNavigationStateChange={(navState) => {
-          console.log(navState.url);
-          console.log(navState.loading);
-        }}
-        onContentProcessDidTerminate={(syntheticEvent) => {
-          const { nativeEvent } = syntheticEvent;
-          console.log('Content process terminated, reloading', nativeEvent);
-        }}
-        originWhitelist={['*']}
-        onError={(syntheticEvent) => {
-          const { nativeEvent } = syntheticEvent;
-          console.warn('WebView error: ', nativeEvent);
-        }}
-        onMessage={(event) => {
-          showMessageModal(event.nativeEvent.data);
-        }}
-        onLoadEnd={data => {
-          const { nativeEvent } = data;
-          const { title } = nativeEvent;
-
-          if (!title.trim()) {
-            // webViewRef.current?.stopLoading();
-            // webViewRef.current?.reload();
-            // setKey(generateRandomKey());
-          }
-        }}
+      {webViewVisible && (
+        <WebView
+          source={{ uri: 'https://demo.ipolyv.cn/tanglianghong/test/v3/ReactNativeWebView/?channelId=5005175&lang=en&console=1' }}  
+          key={key}
+          ref={webViewRef}
+          scalesPageToFit={true}
+          style={{ flex: 1, backgroundColor: 'white' }}
+          startInLoadingState={true}
+          limitsNavigationsToAppBoundDomains={true}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          allowsLinkPreview={true}
+          setSupportMultipleWindows={false}
+          onShouldStartLoadWithRequest={(r) => {
+            return true;
+          }}
+          onNavigationStateChange={(navState) => {
+            console.log(navState.url);
+            console.log(navState.loading);
+          }}
+          onContentProcessDidTerminate={(syntheticEvent) => {
+            const { nativeEvent } = syntheticEvent;
+            console.log('Content process terminated, reloading', nativeEvent);
+          }}
+          originWhitelist={['*']}
+          onError={(syntheticEvent) => {
+            const { nativeEvent } = syntheticEvent;
+            console.warn('WebView error: ', nativeEvent);
+          }}
+          onMessage={(event) => {
+            showMessageModal(event.nativeEvent.data);
+          }}
+          onLoadEnd={data => {
+            const { nativeEvent } = data;
+            const { title } = nativeEvent;
+          }}
+        />
+      )}
+      <Button
+        title="reloadpage"
+        style={styles.loadPageButton}
+        onPress={loadPage}
       />
     </SafeAreaView>
   );
@@ -104,6 +103,12 @@ const styles = StyleSheet.create({
   modalText: {
     marginBottom: 20,
     textAlign: 'center',
+  },
+  loadPageButton: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: [{ translateX: -50 }, { translateY: -50 }],
   },
 });
 
